a76f5e6e9c74f00772a0b32d0abb6dff1ee026a7
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-05-03 22:55:16 +0300

    create turtle instance

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Turtle Crossing")
+screen.tracer(0)
 
+turtle = Turtle(shape="turtle")
+turtle.color("white")
+turtle.penup()
+turtle.setheading(90)
+screen.update()
 
 screen.exitonclick()
